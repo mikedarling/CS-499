c7831f6228ef65a8750ea800889d335a3325a70f
Mike Darling <mike.darling1@gmail.com>
2022-08-13 00:50:59 -0400

    Milestone 2 - Service Layer

---
+++ src/Milestone-2/AnimalRescue/src/Services/AnimalRescue.Services/Animals/AnimalService.cs
@@ -0,0 +1,47 @@
+﻿using AnimalRescue.Data.Models.DomainModels;
+using AnimalRescue.Data.Models.Entities;
+using AnimalRescue.Data.Repositories;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalRescue.Services.Animals
+{
+    public class AnimalService : IAnimalService
+    {
+
+        #region Constructors
+
+        public AnimalService(IDataRepository repository, IMapper mapper)
+        {
+            this._repository = repository;
+            this._mapper = mapper;
+        }
+
+        #endregion
+
+        #region Local Variables
+
+        private readonly IDataRepository _repository;
+
+        private readonly IMapper _mapper;
+
+        #endregion
+
+        #region Methods
+
+        public IEnumerable<AnimalModel> GetAnimals()
+        {
+            var entities = this._repository.GetRecords<Animal>();
+            if (entities == null || !entities.Any())
+            {
+                return null;
+            }
+
+            return this._mapper.Map<IEnumerable<AnimalModel>>(entities);
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Milestone-2/AnimalRescue/src/Services/AnimalRescue.Services/Animals/IAnimalService.cs
@@ -0,0 +1,10 @@
+﻿using AnimalRescue.Data.Models.DomainModels;
+using System.Collections.Generic;
+
+namespace AnimalRescue.Services.Animals
+{
+    public interface IAnimalService
+    {
+        IEnumerable<AnimalModel> GetAnimals();
+    }
+}
